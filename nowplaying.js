f5314a2b778107debdaf42955541fa41ef12d9ae
Skyleter <mogok1penguin@gmail.com>
2016-06-14 22:56:51 -0300

    Update nowplaying.js

--- nowplaying.js
+++ nowplaying.js
@@ -1,7 +1,10 @@
-var seconds = 5; // seconds to check the current song
+// include: <script src="nowplaying.js"></script> - Please include jquery's library first.
+
+var seconds = 5; // seconds to check the current song -- interval
 var time = seconds*1000; // dont edit this.
-var message = 'Now Playing'; // default 'Now Playing:'
-$('.nowplaying').html(message+' : <b><span id="np"></span></b>');
+var message = 'Now Playing'; // default 'Now Playing', this will show as 'Now Playing: SONG'
+var nowp = '.nowplaying' // class (.) or id (#)  of the dom element to place the message variable and the current song. For example <div id="np"></div> <--- id: np
+$(nowp).html(message+': <b><span id="np"></span></b>');
 $('#np').load('song.php');
 setInterval(function(){
   $('#np').load('song.php');
