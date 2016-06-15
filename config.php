5a3898a1ff5260c872f07b5fa40898ee05a3a865
Skyleter <mogok1penguin@gmail.com>
2016-06-14 22:29:00 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+/***
+COPYRIGHT THE SKYLETER LTD. COMPANY
+DEVELOPED BY SKYLETER
+  -- PLEASE DONT REMOVE ANY CREDIT IF YOU ARE GOING TO USE OR SHARE OUR CODE, IF SO, YOU CAN BE IN PROBLEMS. --
+ONLY COMPATIBLE WITH SHOUTCAST.
+***/
+
+$np = array(
+'url' => 'radio-link', // no http://, example: myradio.domain.com
+'port' => 8000, // radio port
+'sid' => 1 // stream id, default 1
+);
+
+?>
