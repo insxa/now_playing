ce86925298b71b244f1a1aed0142571e0de0d447
Skyleter <mogok1penguin@gmail.com>
2016-06-14 23:00:48 -0300

    Update song.php

--- song.php
+++ song.php
@@ -5,7 +5,7 @@ DEVELOPED BY SKYLETER
   -- PLEASE DONT REMOVE ANY CREDIT IF YOU ARE GOING TO USE OR SHARE OUR CODE, IF SO, YOU CAN BE IN PROBLEMS. --
 ONLY COMPATIBLE WITH SHOUTCAST.
 ***/
-<?php require('config.php'); 
+ require('config.php'); 
 
 header('Content-Type:text/plain');
 $a = file_get_contents('http://'.$np['url'].':'.$np['port'].'/currentsong?sid='.$np['sid'].'') or die('Error.');
